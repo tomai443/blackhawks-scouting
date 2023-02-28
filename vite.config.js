08fe53216214d17bfb6cf42bd61c3ac26f190e9f
tomai443 <102867493+tomai443@users.noreply.github.com>
2023-02-28 18:18:05 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -25,9 +25,9 @@ export default defineConfig({
     VitePWA({
       includeAssets: ["assets/*", "icons/*"],
       manifest: {
-        name: "Black Hawks Scouting",
+        name: "Kinematic Wolves Scouting",
         short_name: "Scouting",
-        description: "A scouting app by FRC Team 2834",
+        description: "A scouting app for FRC Team 5577 modified from FRC Team 2834",
         theme_color: "#292929",
         background_color: "#292929",
         icons: [
